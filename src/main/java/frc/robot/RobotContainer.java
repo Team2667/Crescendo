8f00e9d25de92a330037551afeb1b4a6c7e6a80c
AppleValley02 <applevalley2667+02@gmail.com>
2024-02-01 19:10:27 -0600

    added auto command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -132,9 +132,8 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-  //  return Autos.exampleAuto(m_exampleSubsystem);
-  return null;
-  }
+  public Command getAutonomousCommand() { 
+    Command aCommand=new DriveFieldRelative(drivetrain, Math.PI, 0.25);
+   return aCommand.withTimeout(3);
+
 }
