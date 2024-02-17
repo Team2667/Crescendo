bb118d468c34257291619303a9792a1ae9f92f9b
AppleValley01 <applevalley2667+01@gmail.com>
2024-02-17 10:32:08 -0600

    vdfdgdfgxczvc

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -1,23 +1,58 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.REVLibError;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Launcher extends SubsystemBase{
     private CANSparkFlex frontWheel;
     private CANSparkFlex backWheel;
+    private RelativeEncoder frontEncoder;
+    private RelativeEncoder backEncoder;
+    private SparkPIDController frontPID;
+    private SparkPIDController rearPID;
+    private double pV = 4e-1;
+	private double iV = 0;
+    private double dV = 0;
+    private double rearspeed;
+    private double frontspeed;
 
     public Launcher() {
         frontWheel=new CANSparkFlex(Constants.LAUNCHER_WHEEL_FRONT, MotorType.kBrushless);
         backWheel=new CANSparkFlex(Constants.LAUNCHER_WHEEL_BACK, MotorType.kBrushless);
+        frontEncoder=frontWheel.getEncoder();
+        backEncoder=backWheel.getEncoder();
+        frontPID=frontWheel.getPIDController();
+        rearPID=backWheel.getPIDController();
+        frontPID.setP(pV);
+        frontPID.setI(iV);
+        frontPID.setD(dV);
+
+        rearPID.setP(pV);
+        rearPID.setI(iV);
+        rearPID.setD(dV);
+        
+
+    }
+
+    public void setSpeedPid(double front, double rear)
+    {
+        frontPID.setReference(front, CANSparkBase.ControlType.kVelocity);
+        rearPID.setReference(rear, CANSparkBase.ControlType.kVelocity );
+
     }
 
     public void launchToSpeaker() {
         frontWheel.set(0.3);
         backWheel.set(0.3);
+    
     }
     public void frenchMode()
     {
@@ -34,5 +69,12 @@ public class Launcher extends SubsystemBase{
     public void stop() {
         frontWheel.stopMotor();
         backWheel.stopMotor();
+    }
+    @Override
+    public void periodic()
+    {
+        SmartDashboard.putNumber("front wheel",frontEncoder.getVelocity());
+        SmartDashboard.putNumber("rear wheel",backEncoder.getVelocity());
+
     }
 }
