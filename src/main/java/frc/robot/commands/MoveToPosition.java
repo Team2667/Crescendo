8037ab0dc151deab8b988ea45d0ceb82d4883a3e
kentbraaten <kentbraaten@frontiernet.net>
2024-02-24 15:50:50 -0600

    Use supplier in MoveToPositionCommand

--- src/main/java/frc/robot/commands/MoveToPosition.java
+++ src/main/java/frc/robot/commands/MoveToPosition.java
@@ -1,8 +1,9 @@
 package frc.robot.commands;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
@@ -11,9 +12,7 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.PoseEstimatorSubsystem;
 
 public class MoveToPosition extends Command {
     private static TrapezoidProfile.Constraints X_CONSTRAINTS = new TrapezoidProfile.Constraints(3,2 );
@@ -24,17 +23,18 @@ public class MoveToPosition extends Command {
         new Transform3d(
             new Translation3d(1.5, 0.0, 0.0),
             new Rotation3d(0.0, 0.0, Math.PI));
-    private static DriveTrain driveTrain;
-    private static PoseEstimatorSubsystem poseEstimatorSubsystem;
-    private static ProfiledPIDController xController = new ProfiledPIDController(3, 0, 0, X_CONSTRAINTS);
-    private static ProfiledPIDController yController = new ProfiledPIDController(3, 0, 0, Y_CONSTRAINTS);
-    private static ProfiledPIDController omegaController = new ProfiledPIDController(3, 0, 0, OMEGA_CONSTRAINTS);
+    private  DriveTrain driveTrain;
+    private  ProfiledPIDController xController = new ProfiledPIDController(3, 0, 0, X_CONSTRAINTS);
+    private  ProfiledPIDController yController = new ProfiledPIDController(3, 0, 0, Y_CONSTRAINTS);
+    private  ProfiledPIDController omegaController = new ProfiledPIDController(3, 0, 0, OMEGA_CONSTRAINTS);
+    private  Supplier<Pose2d> robotPoseSupplier;
     private Pose2d goalPose;
 
 
-    public MoveToPosition(DriveTrain driveTrain, PoseEstimatorSubsystem poseEstimatorSubsystem) {
+    public MoveToPosition(DriveTrain driveTrain, Supplier<Pose2d> robotPoseSupplier) {
         this.driveTrain = driveTrain;
-        this.poseEstimatorSubsystem = poseEstimatorSubsystem;
+        this.robotPoseSupplier = robotPoseSupplier;
+
 
         xController.setTolerance(0.2);
         yController.setTolerance(0.2);
@@ -48,7 +48,7 @@ public class MoveToPosition extends Command {
     @Override
     public void initialize() {
         //lastTarget
-        var robotPose = poseEstimatorSubsystem.getPosition();
+        var robotPose = robotPoseSupplier.get();
         omegaController.reset(robotPose.getRotation().getRadians());
         xController.reset(robotPose.getX());
         yController.reset(robotPose.getY());
@@ -60,7 +60,7 @@ public class MoveToPosition extends Command {
 
     @Override
     public void execute() {
-        var robotPose = poseEstimatorSubsystem.getPosition();
+        var robotPose = robotPoseSupplier.get();
         var xSpeed = xController.atGoal() ? 0 : xController.calculate(robotPose.getX());
         var ySpeed = yController.atGoal() ? 0 : yController.calculate(robotPose.getY());
         var omegaSpeed = omegaController.atGoal() ? 0 : omegaController.calculate(robotPose.getRotation().getRadians());
