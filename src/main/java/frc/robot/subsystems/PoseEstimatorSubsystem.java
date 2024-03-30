401bd94018ac44d596cb4bcb0d256fede6680de3
AppleValley02 <applevalley2667+02@gmail.com>
2024-03-30 17:02:32 -0500

    things

--- src/main/java/frc/robot/subsystems/PoseEstimatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/PoseEstimatorSubsystem.java
@@ -3,13 +3,18 @@ package frc.robot.subsystems;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
 
+import java.lang.Math;
+
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.proto.Wpimath;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -28,7 +33,7 @@ public class PoseEstimatorSubsystem extends SubsystemBase {
         this.poseEstimator = new SwerveDrivePoseEstimator(driveTrain.getKinematics(), driveTrain.getGyroscopeRotation(), 
                                 driveTrain.getSwerveModulePositions(), new Pose2d());
         aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
-        cameraToRobot = new Transform3d(0,0,0,new Rotation3d());
+        cameraToRobot = new Transform3d(0.0,-0.09525,-1,new Rotation3d()); //fill in Z later
     }
 
     public Pose2d getPosition() {
@@ -42,6 +47,23 @@ public class PoseEstimatorSubsystem extends SubsystemBase {
         displayCurrentPosition();
        }
     }
+
+    private Pose2d camToMid(Pose2d campos){
+        double cr = campos.getRotation().getRadians(); //modify this to actually translate the coords
+
+        double ty = -0.09525 * Math.sin(cr); // 3.75in = 0.09525m
+        double tx = -0.09525 * Math.cos(cr);
+        
+        return campos.transformBy(new Transform2d(tx, ty, new Rotation2d(0)));
+
+    }
+     //MEASURE NOTES
+     //17.5 from edge for middle of robit longways
+     // 21.25 camera
+     // that means its 3.75 inches off
+     // OH MY LORT I NEED TO TAKE INTO ACCOUNT DIRECTION
+     // longways it is 35in/0.889m
+     // shortways it is 29.5in/0.7493m
 
     private void displayCurrentPosition(){
         var position = poseEstimator.getEstimatedPosition();
@@ -62,6 +84,7 @@ public class PoseEstimatorSubsystem extends SubsystemBase {
                     targetPos -> {
                         Transform3d camToTarget = target.getBestCameraToTarget();
                         Pose3d camPose = targetPos.transformBy(camToTarget.inverse());
+                        //FIGGER OUT HOW TO GET THE 3D ROTATION
                         var visionMeasurement = camPose.transformBy(cameraToRobot);
                         poseEstimator.addVisionMeasurement(visionMeasurement.toPose2d(), previousTimeStampSeconds);
                     });
